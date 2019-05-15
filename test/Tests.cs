84b9287ad02fabcaf549f647816da0079a1dbabc
synopia <paul.fritsche@gmail.com>
2019-05-15 10:52:29 +0200

    New project structure

---
+++ test/Tests.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+using VL.NewAudio;
+
+namespace NewAudioTest
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void Test1()
+        {
+            var x = new CircularSampleBuffer(4000);
+            Assert.AreEqual(4000, x.Count);
+        }
+    }
+}
