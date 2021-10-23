34d977b6a1febef93a1696207681a74ff31f7250
synopia <paul.fritsche@gmail.com>
2021-10-23 18:18:41 +0200

    NUnitLite

---
+++ src/NewAudioTest/Main.cs
@@ -0,0 +1,12 @@
+﻿using NUnitLite;
+
+namespace NewAudioTest
+{
+    public static class Program
+    {
+        public static int Main(string[] args)
+        {
+            return new AutoRun().Execute(args);
+        }
+    }
+}
